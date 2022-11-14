e3e779ff9a1046c529526f7c0b5cf5483b3d28fc
Florian Briand <briand.flo13@gmail.com>
2022-11-14 10:48:28 +0100

    New keys

--- gestionCertificat/creation_root_ca.py
+++ gestionCertificat/creation_root_ca.py
@@ -1,6 +1,5 @@
 import os
 # créer une AC avec une configuration bien choisie
 # créer un certificat racine
+# PASS PHRASE : TOTO
 os.system("openssl req -x509 -config root-ca-cert.cnf -newkey rsa:2048 -out ca.pem -keyout private/private.pem -days 1826")
-
-
