60ad2e05fefdd90c9d2b0e784ffced3386762086
Florian Briand <briand.flo13@gmail.com>
2022-11-14 14:09:51 +0100

    Ajout de lien

--- test.py
+++ test.py
@@ -4,7 +4,46 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return """
+    <html>
+    <head>
+    <title>Test</title>
+    </head>
+    <body>
+    <h1>Diplôme digital</h1>
+    <a href="/creerDiplome">Créer un diplôme</a>
+    <br>
+    <a href="/verifDiplom">Vérifier un diplôme</a>
+    </body>
+    </html>
+    """
+@app.route("/creerDiplome")
+def creerDiplome():
+    return """
+    <html>
+    <head>
+    <title>Test</title>
+    </head>
+    <body>
+    <h1>Créer un diplôme</h1>
+    <a href="/">Retour</a>
+    </body>
+    </html>
+    """
+
+@app.route("/verifDiplom")
+def verifDiplom():
+    return """
+    <html>
+    <head>
+    <title>Test</title>
+    </head>
+    <body>
+    <h1>Vérifier un diplôme</h1>
+    <a href="/">Retour</a>
+    </body>
+    </html>
+    """
 
 if __name__ == "__main__":
         app.run(host='127.0.0.1', port=8080, debug=True)
