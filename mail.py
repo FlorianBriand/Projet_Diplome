7c6b1078a4bf08765acf3103807c6ba9f034cc45
Florian B <briand.flo13@gmail.com>
2022-11-23 23:06:53 +0100

    Fonctionnel uniquement si on suprrimer les retraits à la ligne à la main

--- mail.py
+++ mail.py
@@ -2,7 +2,8 @@ import base64
 import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+
+from outils.writeFile import verifFichierExiste
 
 CHEMIN_ACCES_OPENSSL = "C:\\MesProgrammes\\OpenSSL-Win64\\bin\\openssl.exe"
 MAIL_FROM = "projetcryptofloraph@gmail.com"
@@ -12,41 +13,82 @@ COMMANDE_AFFICHE = "type"
 CERTIFICAT = "gestionCertificat/ca.pem"
 
 
-def envoiMail():
-    # encode image to base64
+def affichageErreurSignatureMail(resultatEnvoieMail):
+    if (resultatEnvoieMail == 0):
+        print("Création du contenu sécurisé réussi")
+    elif (resultatEnvoieMail == 1):
+        print("an error occurred parsing the command options.")
+    elif (resultatEnvoieMail == 2):
+        print("an error occurred reading the input file.")
+    elif (resultatEnvoieMail == 3):
+        print("an error occurred creating the PKCS#7 file or when reading the MIME message.")
+    elif (resultatEnvoieMail == 4):
+        print("an error occurred decrypting or verifying the message.")
+    else:
+        print("the message was verified correctly but an error occurred writing out the signers certificates.")
 
-    # Création du message
-    msg = MIMEMultipart()
-    msg['From'] = 'projetcryptofloraph@gmail.com'
-    msg['To'] = 'coutinhora@cy-tech.fr'
 
+def envoiMail(msg):
+    verifFichierExiste("contenu_courrier.txt")
+    with open("contenu_courrier.txt", "r") as contenu_courrier:
+        mail = contenu_courrier.read()
+        contenu_courrier.close()
+    mailserver = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    mailserver.login(msg['From'], 'jrvzgmrmtsotmzct')
+    # Verifie si le mail est envoyé
+    resultatEnvoieMail = mailserver.sendmail(msg['From'], msg['To'], mail)
+    if resultatEnvoieMail != {}:
+        print("Erreur lors de l'envoie du mail")
+    else:
+        print("Mail envoyé avec succès")
+
+    mailserver.quit()
+
+
+def lectureContenuMail():
+    verifFichierExiste(STEGANO_FILE)
     with open(STEGANO_FILE, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
         image_file.close()
 
     with open("contenu.txt", "w") as contenu:
-        contenu.write("Content-Type: image/png\r\nContent-Transfer-Encoding: base64\r\n\r\n" + str(encoded_string))
+        messageContenu = str(encoded_string)
+        # Enlever les caractères b' et ' de la chaine de caractère
+        messageContenu = messageContenu[2:-1]
+
+        contenu.write("Content-Type: image/png\r\nContent-Transfer-Encoding: base64\r\n\r\n" + messageContenu)
         # fermeture du fichier
         contenu.close()
-        
-        commande = COMMANDE_AFFICHE+" contenu.txt | " + CHEMIN_ACCES_OPENSSL + " smime -passin pass:toto -signer "+ CERTIFICAT +" -from '" + MAIL_FROM + "' -to '" + MAIL_TO + "' -subject 'Envoi certificat' -sign -inkey gestionCertificat/private/private.pem -out contenu_courrier.txt"
-        print(commande)
-        os.system(commande)
 
-    with open("contenu_courrier.txt", "r") as contenu_courrier:
-        mail = contenu_courrier.read()
-    contenu_courrier.close()
 
-    # Envoi du mail
-    mailserver = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    mailserver.login(msg['From'], 'jrvzgmrmtsotmzct')
-    mailserver.sendmail(MAIL_FROM, MAIL_TO, mail)
-    mailserver.quit()
+def envoiMailSecurise():
+    # encode image to base64
 
-    # os.system("cat contenu.txt")
+    # Création du message
+    msg = MIMEMultipart()
+    msg['From'] = MAIL_FROM
+    msg['To'] = MAIL_TO
+
+    #lectureContenuMail()
+
+    commande = CHEMIN_ACCES_OPENSSL + \
+               " smime -sign -in contenu.txt -out contenu_courrier.txt -signer " + \
+               CERTIFICAT + " -inkey gestionCertificat/private/private.pem" \
+                            " -passin pass:toto -from '" + MAIL_FROM + "' -to '" + MAIL_TO + "' -subject 'Envoicertificat'"
+
+    print(commande)
+
+    resultatEnvoieMail = os.system(commande)
+
+    affichageErreurSignatureMail(resultatEnvoieMail)
+
+    envoiMail(msg)
+
+    # Nettoyage des fichiers
+    #os.remove("contenu.txt")
+    os.remove("contenu_courrier.txt")
 
 
 if __name__ == '__main__':
     # envoyer un mail automatiquement
-    envoiMail()
-    print('Mail envoyé')
+    envoiMailSecurise()
